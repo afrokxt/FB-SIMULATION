955a86ab03c29730cc983a974115d44b9cff4d83
AsiyaKokayi <AsiyaKokayi@DESKTOP-TVM14LD>
2018-07-22 20:40:14 -0400

    second push

--- logreg.php
+++ logreg.php
@@ -4,20 +4,25 @@ include 'config.php';
 include 'query.php';
 
 //Register
-$email = _POST['email'];
-$password = md5(_POST['password']);
-$name = _POST['name'];
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+$name = $_POST['name'];
 
+echo “<h3> Welcome: “$FirstName $LastName ”</h3>”
+echo “<h3> Email:  “$LastName ”</h3>”
+echo “<h3> First Name: “$Email”</h3>”
 
 //login
 $_SESSION[“FirstName”];
 $fetch = mysqli_fetch_assoc($conn,"SELECT * FROM UserProfile where $_POST('email' && 'password') != 0 ");
     
 function login()
-if fetch= mysqli_fetch_assoc()
 
         
-      switch ($submit)
+      switch ($submit){
         case 1: register; 
-            or 
+          break;
         case 2: login;
+            break;
+            
+      }
